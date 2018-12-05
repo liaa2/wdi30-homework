aa4df38da0986a6153d386b89496b5cad5a7afd0
JETZ <jeremyetzine@hotmail.com>
2018-12-05 11:04:58 +1100

    calculator and strings

---
+++ jeremy_etzine/week01/tuesday/homework-2.js
@@ -0,0 +1,32 @@
+/*
+DrEvil
+Create a function called DrEvil. It should take a single argument, an amount,
+and return ' dollars', except it will add '(pinky)' at the end if the amount is 1 million.
+*/
+
+// const drEvil = function(num1){
+//   let result = `${num1} dollars`;
+//   if (result >= 1000000) {
+//     result = `${result} dollars (pinky)`;
+//   }
+//   return result;
+// }
+// const answer = drEvil(1000001)
+// console.log(answer);
+
+
+/*
+Create a function called mixUp. It should take in two strings, and return the concatenation
+of the two strings (separated by a space) slicing out and swapping the first 2 characters of
+each. You can assume that the strings are at least 2 characters long.
+*/
+
+const mixUp = function(string1, string2){
+  const string3 = string1.replace(/st/g, "")
+  const result = ``;
+  return result;
+}
+
+// var str = "Visit Microsoft!";
+// var res = str.replace("Microsoft", "W3Schools");
+// console.log(res);

c4f1721420bc594c5dcb84da99768260524db8e6
Elli <ellimoty@gmail.com>
2018-12-09 14:08:41 +1100

    final try - MTA Lab

--- elli_motaghi/week01/friday - 5th day/MTA Lab/MTA.js
+++ elli_motaghi/week01/friday - 5th day/MTA Lab/MTA.js
@@ -10,8 +10,6 @@
 // // "Your journey continues through the following stops: 23rd, 28th, 33rd."
 // // "7 stops in total."
 
-
-
 // There are 3 subway lines:
 // The N line has the following stops: Times Square, 34th, 28th, 23rd, Union Square, and 8th
 // The L line has the following stops: 8th, 6th, Union Square, 3rd, and 1st
@@ -19,7 +17,6 @@
 
 // All 3 subway lines intersect at Union Square, but there are no other intersection points. (For example, this means the 28th stop on the N line is different than the 28th street stop on the 6 line, so you'll have to differentiate this when you name your stops in the arrays.)
 // Tell the user the number of stops AND the stops IN ORDER that they will pass through or change at.
-
 
 // Hints:
 // Work out how you would do it on paper first! Then start to explain that process in Javascript.
@@ -33,46 +30,51 @@
 // Make sure the stops that are the same for different lines have different names (i.e. 23rd on the N and on the 6 need to be differentiated)
 
 
+///// naming correctly is bloody important /////////////////////////////////////////////////////////////
+
 const subwaySystem = [
 
-    { line: 'N', stops: ['Times Square', '34th', '28th', '23rd', 'Union Square', '8th'] },
-    { line: 'L', stops: ['8th', '6th', 'Union Square', '3rd', '1st'] },
-    { line: '6', stops: ['Grand Central', '33rd', '28th', '23rd', "Union Square", 'Astor Place'] }
+    { lineName: 'N', stops: ['Times Square', '34th', '28th', '23rd', 'Union Square', '8th'] },
+    { lineName: 'L', stops: ['8th', '6th', 'Union Square', '3rd', '1st'] },
+    { lineName: '6', stops: ['Grand Central', '33rd', '28th', '23rd', "Union Square", 'Astor Place'] }
 
 ];
 
+function findLine(lineName) { 
+    for (let i = 0; i < subwaySystem.length; i++) {
+        if (subwaySystem[i].lineName === lineName) {
+            return subwaySystem[i];
+        }
+    } // line = subwaySystem[i]
+}
 
-function moveOnOneLine(line, start, stop) {
+function moveForward(line, start, stop, tripStops) {
+    for (let i = line.stops.indexOf(start) + 1; i <= line.stops.indexOf(stop); i++) {
+        tripStops.push(line.stops[i]);
+    }
+}
+
+function moveBackward(line, start, stop, tripStops) {
+    for (let i = line.stops.indexOf(start) - 1; i >= line.stops.indexOf(stop); i--) {
+        tripStops.push(line.stops[i]);
+    }
+}
+
+
+function moveOnOneLine(lineName, start, stop) {
     let tripStops = [];
     let totalStops;
 
-    for (i = 0; i < subwaySystem.length; i++) {
-        if (subwaySystem[i].line === line) {
-            if (subwaySystem[i].stops.indexOf(start) < subwaySystem[i].stops.indexOf(stop)) {
-                for (j = subwaySystem[i].stops.indexOf(start) + 1; j <= subwaySystem[i].stops.indexOf(stop); j++) {
-                    tripStops.push(subwaySystem[i].stops[j]);
-                }
-            } 
-            
-            else {
-                for (j = subwaySystem[i].stops.indexOf(start) - 1; j >= subwaySystem[i].stops.indexOf(stop); j--) {
-                    tripStops.push(subwaySystem[i].stops[j]);
-                }
-            }
-        }
+    const line = findLine(lineName);
+    if (line.stops.indexOf(start) < line.stops.indexOf(stop)) {
+        moveForward(line, start, stop, tripStops);
+    } else {
+        moveBackward(line, start, stop, tripStops);
     }
 
     totalStops = tripStops.length;
     return { tripStops, totalStops };
 }
-
-// console.log(moveOnOneLine('6', 'Grand Central', 'Astor Place'));
-// console.log(moveOnOneLine('6', 'Astor Place', 'Grand Central'));
-// console.log(moveOnOneLine('6', 'Union Square', '33rd'));
-// console.log(moveOnOneLine('6', '33rd', 'Union Square'));
-
-
-
 
 
 function playTrip(startLine, startStop, endLine, endStop) {
@@ -83,7 +85,7 @@ function playTrip(startLine, startStop, endLine, endStop) {
         const moveOnOneLineResult = moveOnOneLine(startLine, startStop, endLine);
         message = `You must travel through the following stops on the ${startLine} line: ${moveOnOneLineResult.tripStops.join(', ')}. ${moveOnOneLineResult.totalStops} stops in total.`;
 
-    } 
+    }
 
     else {
         const moveOnFirstLine = moveOnOneLine(startLine, startStop, 'Union Square');
@@ -101,11 +103,17 @@ function playTrip(startLine, startStop, endLine, endStop) {
 // console.log(playTrip('6', '33rd', 'L', '6th'));
 
 // console.log(playTrip('N', '34th', '6', 'Astor Place'));
-//console.log(playTrip('N', 'Times Square', '6', '33rd'));
+// console.log(playTrip('N', 'Times Square', '6', '33rd'));
 //console.log(playTrip('L', '6th', 'L', '1st'));
-//console.log(playTrip('N', 'Times Square', 'N', '8th'));
+// console.log(playTrip('N', 'Times Square', 'N', '8th'));
 //console.log(playTrip('L', 'Union Square', 'L', '1rd'));
 // console.log(playTrip('6', '33rd', '6', 'Astor Place'));
 //console.log(playTrip('6', '33rd', '6', 'Union Square'));
 
-//console.log(playTrip('L', '8th', '6', 'Astor Place'));
+// console.log(playTrip('L', '8th', '6', 'Astor Place'));
+
+// console.log(moveOnOneLine('6', 'Grand Central', 'Astor Place'));
+// console.log(moveOnOneLine('6', 'Astor Place', 'Grand Central'));
+// console.log(moveOnOneLine('6', 'Union Square', '33rd'));
+// console.log(moveOnOneLine('6', '33rd', 'Union Square'));
+

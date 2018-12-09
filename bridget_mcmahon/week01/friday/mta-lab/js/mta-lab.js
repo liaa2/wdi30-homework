f1aa71cc2ddec7be60d119f5ed49d48729b03bf9
Mossy <bridgo.mcmahon@gmail.com>
2018-12-09 19:41:53 +1100

    added logic to planTrip function

--- bridget_mcmahon/week01/friday/mta-lab/js/mta-lab.js
+++ bridget_mcmahon/week01/friday/mta-lab/js/mta-lab.js
@@ -1,24 +1,21 @@
 /*
 Create a program that models a simple subway system.
 
-The program takes the line and stop that a user is getting
-on at and the line and stop that user is getting off at and
-prints the journey and the total number of stops for the trip
-in the console.
+The program takes the line and stop that a user is getting on at and the line
+and stop that user is getting off at and prints the journey and the total
+number of stops for the trip in the console.
 
 There are 3 subway lines:
 The N line has the following stops: Times Square, 34th, 28th, 23rd, Union Square, and 8th
 The L line has the following stops: 8th, 6th, Union Square, 3rd, and 1st
 The 6 line has the following stops: Grand Central, 33rd, 28th, 23rd, Union Square, and Astor Place.
 
-All 3 subway lines intersect at Union Square, but there are no other
-intersection points. (For example, this means the 28th stop on the N line
-is different than the 28th street stop on the 6 line, so you'll have to
-differentiate this when you name your stops in the arrays.)
+All 3 subway lines intersect at Union Square, but there are no other intersection
+points. (For example, this means the 28th stop on the N line is different than the
+28th street stop on the 6 line, so you'll have to differentiate this when you name
+your stops in the arrays.)
 
-Tell the user the number of stops AND the stops IN ORDER that they
-will pass through or change at.
-
+Tell the user the number of stops AND the stops IN ORDER that they will pass through or change at.
 */
 
 const subwaySystem = {
@@ -28,53 +25,47 @@ const subwaySystem = {
 };
 
 const planTrip = function(startLine, startStation, endLine, endStation) {
-  const lineOneStops = [];
-  const lineTwoStops = [];
   // variables for positions on lines travelled
   const startPosition = subwaySystem[startLine].indexOf(startStation);
-  const endPosition = subwaySystem[endLine].indexOf(endStation);
   const lineOneEnd = subwaySystem[startLine].indexOf("Union Square");
   const lineTwoStart = subwaySystem[endLine].indexOf("Union Square");
+  const endPosition = subwaySystem[endLine].indexOf(endStation);
 
-  const addStops = function (start, end) {
-    if (start < end) {
+  const lineOneStops = [];
+  const lineTwoStops = [];
+  let userMessage = "";
+
+  const addStops = function (start, end, stops, line) {
+    if ( start < end ) {
       for (let i = start + 1; i <= end; i++) {
-        lineOneStops.push(subwaySystem[startLine][i]);
+        stops.push(subwaySystem[line][i]);
       }
     }
-    if (end < start) {
+    if ( end < start ) {
       for (let i = start - 1; i >= end; i--) {
-        lineOneStops.push(subwaySystem[startLine][i]);
+        stops.push(subwaySystem[line][i]);
       }
     }
   };
 
-  // if not travelling on different lines -----------------------------
-  if ( startLine === endLine ) {
-    addStops(startPosition, endPosition);
-
-    // list stops on first line
-    let lineOneStopsStr = lineOneStops.join(', ');
-    console.log(`You must travel through the following stops on the ${ startLine } line: ${ lineOneStopsStr }`);
-
+  if ( startLine === endLine && startStation === endStation ) {
+    return "You are already at the right station."
+  } else if (startPosition === -1 || endPosition === -1) {
+    return "Please enter a valid station";
+  // if journey is on one line ----------------------------------------
+  } else if ( startLine === endLine ) {
+    addStops(startPosition, endPosition, lineOneStops, startLine);
+    userMessage += `You must travel through the following stops on the ${ startLine } line: ${ lineOneStops.join(', ') }`;
   // if you need to change stations -----------------------------------
   } else {
-    addStops(startPosition, lineOneEnd);
-
-    lineOneStopsStr = lineOneStops.join(', ');
-    console.log(`You must travel through the following stops on the ${ startLine } line: ${ lineOneStopsStr }`);
-    console.log(`Change stations at Union Square and get on line ${ endLine }`);
+    addStops(startPosition, lineOneEnd, lineOneStops, startLine);
+    userMessage += `You must travel through the following stops on the ${ startLine } line: ${ lineOneStops.join(', ') } \nChange stations at Union Square and get on line ${ endLine }\n`;
 
     // continue journey on new line
-    addStops(lineTwoStart, endPosition);
-
-    lineTwoStopsStr = lineTwoStops.join(', ');
-    console.log(`Your journey continues through the following stops: ${ lineTwoStopsStr }`);
+    addStops(lineTwoStart, endPosition, lineTwoStops, endLine);
+    userMessage += `Your journey continues through the following stops: ${ lineTwoStops.join(', ') }\n`;
   };
-
   let totalStops = lineOneStops.length + lineTwoStops.length;
-  console.log(`${ totalStops } stops in total.`)
+  userMessage += `${ totalStops } stops in total.`;
+  return userMessage;
 };
-
-// next challenge: generate return messages if the stops are the same or
-// if you enter a stop/line that isn't valid, return error message

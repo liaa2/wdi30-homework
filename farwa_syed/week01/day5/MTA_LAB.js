961d081d130d70674db2c8bde66dcc7c6e6563ee
Farwa Abid <farwaabid@Farwas-MacBook-Pro.local>
2018-12-11 08:00:41 +1100

    homework for week1

--- farwa_syed/week01/day5/MTA_LAB.js
+++ farwa_syed/week01/day5/MTA_LAB.js
@@ -5,23 +5,50 @@ let Line6 = ["Grand Central", "33rd", "28th6", "23rd6", "Union_Square", "Astor P
 
 const centralStation = "Union_Square";
 //let departureStation = deptst;
-const mta = function (line, destst, deptst) {
-  if (deptst === LineN){
-    //console.log(`test ${deptst}`);
-      for(let i=0; i<LineN.length; i++)
-        if (deptst === LineN[i]){
-            let numOfStations = LineN.indexOf(centralStation) - LineN.indexOf(deptst);
-            if (numOfStations > 0){
-              } else if (numOfStations < 0) {
-                numOfStations = Math.abs(numOfStations);
-                console.log(`Your destination station is ${numOfStations} stations going forward from Union Square on line N.`);
-              }
-              console.log( `Number of stations to travel ${numOfStations}` );
-            }
+const mta = function (line, destst, departureStation) {
+  if (departureStation === LineN){
+    // for(let i=0; i<LineN.length; i++)
+    //   if (departureStation === LineN[i]) {
+    //     let numOfStations = LineN.indexOf(centralStation) - LineN.indexOf(departureStation);
+    //       if (numOfStations > 0) {
+    //         console.log(`Your destination station is ${numOfStations} stations going forward from Union Square on line N.`);
+    //       } else if (numOfStations < 0) {
+    //         numOfStations = Math.abs(numOfStations);
+            console.log( `Number of stations to travel ${numOfStations}` );
 
           }
-        }
-        console.log(`lineL`);
+      //}
+    } else {
+      console.log(`test`);
+    }
+
+//}
+//notes from demos
+
+//current station =! centralStation or keep the true or false statement to check if the central station index matches the entered station or not.
+//break in to small functions to incorp in to one big function
+//function for each line..join string method for strings*
+//------------ 
+//const mta = function (line, destst, departureStation) {
+//     if (departureStation === LineN){
+//         for(let i=0; i<LineN.length; i++)
+//             if (departureStation === LineN[i]){
+//               let numOfStations = LineN.indexOf(centralStation) - LineN.indexOf(departureStation);
+//                 if (numOfStations > 0){
+//                   } else if (numOfStations < 0) {
+//                     numOfStations = Math.abs(numOfStations);
+//                     console.log(`Your destination station is ${numOfStations} stations going forward from Union Square on line N.`);
+//                   }
+//                   console.log( `Number of stations to travel ${numOfStations}` );
+//                 }
+//               }
+//         } else if (departureStation === LineL) {
+//               console.log(`test`);
+//           }
+//         }
+//       }
+//
+//         console.log(`lineL`);
 mta("LineN", "Union_Square", "Times_Square");
 mta("LineL", "Union_Square", "28N");
-mta("N", "Union_Square", "8N");
+mta("LineN", "Union_Square", "8N");

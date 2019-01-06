c49023a17229d4ce88291cb264fed2b3b484f41d
Guanhua <william.ting.1116@gmail.com>
2019-01-07 10:32:20 +1100

    finished warmup

--- william_ding/week04/monday/warmup/js/textSoup.js
+++ william_ding/week04/monday/warmup/js/textSoup.js
@@ -8,6 +8,7 @@ $(document).ready(function () {
     this.fadeInTime = 1000;
     this.fadeOutTime = 2000;
     this.fontSize = 12;
+    this.color = [ 0, 0, 0 ];
   }
 
   let word = new Word();
@@ -15,11 +16,13 @@ $(document).ready(function () {
   gui.add(word, "fadeInTime", 1, 5000);
   gui.add(word, "fadeOutTime", 1, 5000);
   gui.add(word, "fontSize", 1, 40);
+  gui.addColor(word, "color");
 
   const randomWord = function () {
     const random = $("#text-container").text().split(/[ ,.:""\n\-]+/)[Math.floor(Math.random() * $("#text-container").text().split(/[ ,.:""\n\-]+/).length)];
     const $div = $("<div class='word'></div>").html(random);
     $div.css("font-size", word.fontSize);
+    $div.css("color", `rgb(${word.color[0]}, ${word.color[1]}, ${word.color[2]})`);
     $("body").append($div);
     randomPosition($div);
     $div.fadeIn(word.fadeInTime).fadeOut(word.fadeOutTime);

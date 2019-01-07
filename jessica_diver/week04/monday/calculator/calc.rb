86b7c576816dfdcc0a28091597f6571c7c279c59
jessdiv <jessica.diver@hotmail.com>
2019-01-08 10:17:44 +1100

    added sr, x and colors

--- jessica_diver/week04/monday/calculator/calc.rb
+++ jessica_diver/week04/monday/calculator/calc.rb
@@ -1,98 +1,92 @@
 require 'pry'
 require 'rainbow'
-# binding.binding.pry
 
+# display the menu
 def show_menu
-  puts "CALCULATOR"
-  puts "-=" * 30
-  puts "~ Menu ~"
-  puts "[a] - Addition"
-  puts "[s] - Subtract"
-  puts "[m] - Multiply"
-  puts "[d] - Divide"
-  puts "[q] - Quit"
-  print "Enter your selection: "
+  puts ""
+  puts Rainbow("CALCULATOR").plum.underline.bright
+  puts ""
+  puts Rainbow("~ Menu ~").skyblue
+  puts ""
+  puts Rainbow("[a] - Addition").crimson
+  puts Rainbow("[s] - Subtract").crimson
+  puts Rainbow("[m] - Multiply").crimson
+  puts Rainbow("[d] - Divide").crimson
+  puts Rainbow("[sq] - Square").crimson
+  puts Rainbow("[sr] - Square Root").crimson
+  puts Rainbow("[x] - Exponent").crimson
+  puts Rainbow("[q] - Quit").crimson
+  puts ""
+  print Rainbow("Enter your selection: ").skyblue.blink
 end
 
-def add(a, b)
-  a + b
+
+# math methods
+def add(a, b) a + b end
+def subtract(a, b) a - b end
+def multiply(a, b) a * b end
+def divide (a, b) a / b end
+def square (a) a * a end
+def square_root (a) a ** 0.5 end
+def exponent (a, b) a**b end
+
+def get_number()
+  print Rainbow("Enter number: ").skyblue
+  a = gets.to_f
+  return a
 end
 
-def subtract(a, b)
-  a - b
-end
-
-def multiply(a, b)
-  a * b
-end
-
-def divide (a, b)
-  a / b
+def get_answer(answer)
+  puts ""
+  puts Rainbow("ANSWER: #{ answer }").orange
 end
 
 # show menu
+# get the user choice
 
 show_menu
 menu_choice = gets.chomp.downcase
+system('clear')
 
-# get the user choice
-
-puts "You entered: #{ menu_choice } "
+puts ""
+puts Rainbow("You entered: #{ menu_choice } ").plum.bright
 
 # until the user quits
 until menu_choice == 'q'
+
+# determines if 1 or 2 numbers are needed for the calculation
+  if menu_choice == 'sq' || menu_choice == 'sr'
+    number_1 = get_number
+  else
+    number_1 = get_number
+    number_2 = get_number
+  end
+
   case menu_choice
   when 'a'
-
-    print "First number: "
-    number_1 = gets.to_i
-    print "Second number: "
-    number_2 = gets.to_i
     answer = add number_1, number_2
-    puts ""
-    puts "ANSWER: #{ answer }"
-    puts ""
-
   when 's'
-
-    print "First number: "
-    number_1 = gets.to_i
-    print "Second number: "
-    number_2 = gets.to_i
     answer = subtract number_1, number_2
-    puts ""
-    puts "ANSWER: #{ answer }"
-    puts ""
-
   when 'm'
-
-    print "First number: "
-    number_1 = gets.to_i
-    print "Second number: "
-    number_2 = gets.to_i
     answer = multiply number_1, number_2
-    puts ""
-    puts "ANSWER: #{ answer }"
-    puts ""
-
   when 'd'
-
-    print "First number: "
-    number_1 = gets.to_i
-    print "Second number: "
-    number_2 = gets.to_i
     answer = divide number_1, number_2
-    puts ""
-    puts "ANSWER: #{ answer }"
-    puts ""
-
-else
+  when 'x'
+    answer = exponent number_1, number_2
+  when 'sq'
+    answer = square number_1
+  when 'sr'
+    answer = square_root number_1
+  else
   puts "Invalid selection"
 end
 
+  get_answer(answer)
   show_menu
+  puts ""
   menu_choice = gets.chomp.downcase
+  system('clear')
+  puts ""
+  puts "You entered: #{ menu_choice } "
+
 end
-  # do the math
-  # show the menu
-  # get the user choice

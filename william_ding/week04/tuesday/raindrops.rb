e62a9a7aaeb4f44577219988718dbc9010fe6437
Guanhua <william.ting.1116@gmail.com>
2019-01-08 09:12:19 +1100

    finished morning warmup

---
+++ william_ding/week04/tuesday/raindrops.rb
@@ -0,0 +1,12 @@
+print "Enter a number: "
+number = gets.to_f
+
+message = ""
+
+message = message + "Pling " if number % 3 == 0
+message = message + "Plang " if number % 5 == 0
+message = message + "Plong " if number % 7 == 0
+message = number.to_s if number % 3 != 0 && number % 5 != 0 && number % 7 != 0
+
+message = message + "\n"
+print message

85d668daef56466371854a1a7c6a645fe8ca56e6
Elena Ovsyannikova <helenovsau@gmail.com>
2019-01-11 01:14:02 +1100

    homework for week4 day 3-4

--- elena_ovsyannikova/week4/day3/books/main.rb
+++ elena_ovsyannikova/week4/day3/books/main.rb
@@ -9,8 +9,12 @@ get '/search' do
   @title_book = params[:title_of_book]
   url = "https://www.googleapis.com/books/v1/volumes?q=title:#{ @title_book }"
   info=HTTParty.get url
-
+  @author_book=info["items"][0]["volumeInfo"]["authors"][0]
+  @published_Date=info["items"][0]["volumeInfo"]["publishedDate"]
   @image_book=info["items"][0]["volumeInfo"]["imageLinks"]["thumbnail"]
+  @page_Count=info["items"][0]["volumeInfo"]["pageCount"]
+  @language_book=info["items"][0]["volumeInfo"]["language"]
+  @description_book=info["items"][0]["volumeInfo"]["description"]
 
   erb :search
 

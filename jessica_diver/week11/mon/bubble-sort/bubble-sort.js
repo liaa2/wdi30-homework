98177a1ac93e65786ce1103171fef07df9fc42c9
jessdiv <jessica.diver@hotmail.com>
2019-02-26 20:08:33 +1100

    warmups

---
+++ jessica_diver/week11/mon/bubble-sort/bubble-sort.js
@@ -0,0 +1,12 @@
+a = [4, 17, 3, 25, 7, 8, 14, 11, 27, 23, 2]
+
+const bubbleSort = (array) => {
+
+  for (let i = 0; i < array.length -1; i++) {
+    console.log(array[i]);
+    if ( array[i] > array[i + 1])
+  }
+
+};
+
+console.log(bubbleSort(a));
